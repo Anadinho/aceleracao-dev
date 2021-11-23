50329b1d418d9d44132dee492b19382aeff386b8
Gabriel Anadinho Pinheiro <gabrielanadinho@gmail.com>
2021-11-23 17:16:16 -0300

    SOLID

---
+++ PHP_SOLID/exercicio88.php
@@ -0,0 +1,30 @@
+<?php
+
+class Cliente{
+    public function isValid():bool{
+        return true;
+    }
+}
+
+interface ClienteRepository{
+    public function adicionarCliente(Cliente $cliente);
+}
+
+class ClienteService implements ClienteRepository{
+    public function adicionarCliente(Cliente $cliente){
+        if(!$cliente->isValid()){
+            echo "Dados Invalidos";
+        }
+        $repo = new ClienteRepository();
+
+        if($repo ->adicionarCliente($cliente)){
+            echo "Cliente cadastrado com sucesso";
+        }else{
+            echo "Error";
+        }
+    }
+}
+
+
+
+?>
